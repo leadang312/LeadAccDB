611044cb1d3dd7d004d5427dab379989bf99d69b
leadang312 <lea.dang@y42.com>
2023-06-10 19:29:24 +0200

    Download button

--- pages/Data.py
+++ pages/Data.py
@@ -18,3 +18,12 @@ st.caption("Here you can view the data")
 # Database
 lead_db = pd.read_csv("./lead_db.csv", index_col='lead_id')
 st.experimental_data_editor(lead_db)
+
+
+# Download
+@st.cache_data
+def convert_df(df):
+    return df.to_csv().encode('utf-8')
+
+csv = convert_df(lead_db)  
+st.download_button(label="Download",  data=csv, file_name='LeadList.csv', mime='text/csv')
