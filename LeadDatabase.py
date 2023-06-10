731261a4eabd4912ce988b57208b1acc96209813
leadang312 <lea.dang@y42.com>
2023-06-10 18:37:48 +0200

    Created entire list

--- LeadDatabase.py
+++ LeadDatabase.py
@@ -19,7 +19,7 @@ st.caption("Here you can add new leads to the lead database")
 
 # Macros
 lead_db = pd.read_csv("./lead_db.csv", index_col='lead_id')
-required_cols = ["lead_id", "company_id", "campaign_id", "firstname", "lastname", "title", "company", "country", "region", "email", "sf_linkedin_url", "linkedin_url", "rating", "leadsource", "website", "note", "existing_acc"]
+required_cols = ["lead_id", "company_id", "campaign_id", "firstname", "lastname", "title", "company", "country", "region", "email", "mobilephone", "officephone","phone", "sf_linkedin_url", "linkedin_url", "rating", "leadsource", "website", "note", "existing_acc"]
 
 if 'mapped' not in st.session_state:
     st.session_state.mapped = False
@@ -71,8 +71,21 @@ if lead_list is not None:
         
         st.write("")
         st.markdown(f"**3️⃣ THIRD STEP:** Check if every column is renamed correctly.")
-        lead_df = st.experimental_data_editor(lead_df, key = "mapped_df")
 
+        # Preprocess lead_df
+        for col in required_cols:
+
+            if col not in list(lead_df.columns):
+                lead_df[col] = np.nan
+
+        lead_df = lead_df[required_cols].dropna(how='all', axis=1)
+        lead_df = lead_df.reindex(columns=required_cols)
+
+        lead_df['company_id'] = lead_df['company_id'].fillna("")
+        lead_df['company_id'] = lead_df['company_id'].str.replace('www.', '').str.split('/').str[0]
+
+        lead_df = lead_df.replace([""], np.nan)
+        lead_df = st.experimental_data_editor(lead_df, key = "mapped_df")
 
         ### FOURTH STEP: Adding leads to the database
         st.write("")
@@ -84,16 +97,6 @@ if lead_list is not None:
 
 
     if st.session_state.added and st.session_state.mapped:
-
-        # Preprocess lead_df
-        for col in required_cols:
-
-            if col not in list(lead_df.columns):
-                lead_df[col] = np.nan
-
-        lead_df = lead_df[required_cols].dropna(how='all', axis=1)
-        lead_df = lead_df.replace("", np.nan)
-        lead_df = lead_df.reindex(columns=required_cols)
 
         # Adding lead_df to lead_db with progress bar
         progress_text = "Operation in progress. Please wait."
